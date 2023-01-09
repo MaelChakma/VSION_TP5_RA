ecbb740a444abff2638c0ed17879bb0506a11f7a
RomainDEW <Romain.Dewulf@eleves.ec-nantes.fr>
2023-01-09 16:17:52 +0100

    base du main

--- TP5_VSION/TP5_VSION/main.cpp
+++ TP5_VSION/TP5_VSION/main.cpp
@@ -1,3 +1,39 @@
-void main() {
+// Aruco Include
+#include<aruco.h>
 
+// IOpenCV Include
+#include<opencv2\core\core.hpp>
+#include<opencv2\imgproc\imgproc.hpp>
+#include<opencv2\highgui\highgui.hpp>
+
+// Other Includes
+#include<iostream>
+
+// Namespaces
+using namespace std;
+using namespace cv;
+
+// Define esc key code
+#define ESC_KEY 27
+
+// Main function
+void main() {
+	// Variable declaration
+	string imageAdress;
+	string windowName = "Augmented Reality";
+	char key = 'a';
+	Mat image;
+
+	// Asking image name
+	cout << "Adresse de l'image: ";
+	cin >> imageAdress;
+
+	// Reading image
+	image = imread(imageAdress);
+
+	// Printing loop
+	while (key != ESC_KEY) {
+		imshow(windowName, image);
+		key = waitKey();
+	}
 }
